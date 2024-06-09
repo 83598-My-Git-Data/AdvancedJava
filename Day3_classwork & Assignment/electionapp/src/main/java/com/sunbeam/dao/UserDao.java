b8e7a6b658f29029204a05f1c7f6da45126444e1
kd2-Rohit-83598 <rohitjadhav00700@gmail.com>
2024-06-09 19:28:33 +0530

    Day-3

---
+++ Day3_classwork & Assignment/electionapp/src/main/java/com/sunbeam/dao/UserDao.java
@@ -0,0 +1,22 @@
+package com.sunbeam.dao;
+
+import com.sunbeam.entities.User;
+
+public interface UserDao extends AutoCloseable
+{
+	User findByEmail(String email) throws Exception;
+
+	User findById(int id) throws Exception;
+
+	int save(User user) throws Exception;
+
+	int updateStatus(int userId, boolean voted) throws Exception;
+
+	int updatePassword(int userId, String newPassword) throws Exception;
+
+	int deleteById(int id) throws Exception;
+
+	int update(User user) throws Exception;
+
+}	
+
